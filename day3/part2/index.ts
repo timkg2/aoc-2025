2c8673adc31601c8e1f85c909f721b068721f711
Tim KG <tim.theodor@gmail.com>
2025-12-19 09:08:32 +0100

    day 3, part 2

--- day3/part2/index.ts
+++ day3/part2/index.ts
@@ -1,11 +1,58 @@
 import fs from 'node:fs';
 
-export function solve(input: unknown) {
-    return 0;
+export function solve(batteries: string[]) {
+    const joltages = batteries.map(getHighestJoltage);
+    const sum = joltages.reduce((acc, curr) => acc + curr, 0)
+    return sum;
+}
+
+const batteries = getInput('../puzzle-input.txt')
+const sum = solve(batteries)
+console.log('res', sum)
+
+export function getHighestJoltage(battery: string) {
+    const NUM_DIGITS = 12;
+
+    const digits = [];
+    let pos = 0;
+    for (let i = 0; i < NUM_DIGITS; i ++) {
+        const digit = getHighestDigitInRange(pos, battery.length - (NUM_DIGITS - digits.length), battery);
+        digits.push(digit.digit);
+        pos = digit.pos + 1;
+    }
+
+    const res = parseInt(digits.join(''), 10);
+    console.log(`highest: ${res}`)
+
+    return res;
+}
+
+function getHighestDigitInRange(fromIndex: number, toIndex: number, battery: string) {
+    if (toIndex > battery.length) {
+        throw new Error()
+    }
+    if (fromIndex > toIndex) {
+        throw new Error()
+    }
+
+    let highestDigit = battery[fromIndex]!;
+    let highestDigitPos = fromIndex;
+    for (let i = fromIndex + 1; i <= toIndex; i++) {
+        if (battery[i]! > highestDigit) {
+            highestDigit = battery[i]!;
+            highestDigitPos = i;
+        }
+    }
+
+    return {
+        digit: highestDigit,
+        pos: highestDigitPos
+    }
 }
 
 export function getInput(filename: string) {
-    const fileContents = fs.readFileSync(`./${filename}`, { encoding: 'utf8' });
-    
-    return fileContents;
+    const fileContents = fs.readFileSync(filename, { encoding: 'utf8' });
+    const joltages = fileContents.split('\n');
+    return joltages;
 }
+
--- day3/part2/test.ts
+++ day3/part2/test.ts
@@ -1,6 +1,15 @@
-import { solve, getInput } from './index';
+import { solve, getInput, getHighestJoltage } from './index';
 import assert from 'node:assert';
 
-const input = getInput('test-input.ts')
-const res = solve(input);
-assert.equal(res, 0)
+const batteries = getInput('./test-input.ts')
+const res = batteries.map(getHighestJoltage)
+const expected = [
+    987654321111,
+    811111111119,
+    434234234278,
+    888911112111
+]
+assert.deepStrictEqual(res, expected)
+
+const sum = solve(batteries)
+assert.equal(sum, 3121910778619)

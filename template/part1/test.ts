e99dae5627f27b2f49b803699f0f458602bb00fa
Tim KG <tim.theodor@gmail.com>
2025-12-20 16:55:43 +0100

    fix filename in template

--- template/part1/test.ts
+++ template/part1/test.ts
@@ -2,13 +2,13 @@ import { solve, getInput } from './index.ts';
 import assert from 'node:assert';
 
 Deno.test('day x, part y - test input', () => {
-    const input = getInput('./test-input.ts')
+    const input = getInput('./test-input.txt')
     const res = solve(input);
     assert.equal(res, 0)
 })
 
 Deno.test('day x, part y - puzzle input', () => {
-    const input = getInput('../puzzle-input.ts')
+    const input = getInput('../puzzle-input.txt')
     const res = solve(input);
     assert.equal(res, 0)
 })

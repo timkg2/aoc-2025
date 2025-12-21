31940a2336d06a3aac00686e30bbd79be85e1436
Tim KG <tim.theodor@gmail.com>
2025-12-21 11:01:32 +0100

    day 5, part 2

--- day5/part2/index.ts
+++ day5/part2/index.ts
@@ -1,11 +1,168 @@
+ /// <reference lib="deno.ns" />
 import fs from 'node:fs';
 
-export function solve(input: unknown) {
-    return 0;
+type IRange = {
+    lower: number,
+    upper: number
 }
 
-export function getInput(filename: string) {
-    const fileContents = fs.readFileSync(`./${filename}`, { encoding: 'utf8' });
+class Range implements IRange {
+    constructor(
+        public lower: number,
+        public upper: number,
+    ) {}
+
+    toString() {
+        return `[${this.lower}, ${this.upper}]`
+    }
+
+    toJSON() {
+        return { lower: this.lower, upper: this.upper }
+    }
+
+    static fromJSON(rangeJSON: IRange) {
+        return new Range(rangeJSON.lower, rangeJSON.upper);
+    }
+}
+
+type IInput = {
+    ranges: Range[],
+    ids: number[],
+}
+
+export function solve(input: IInput) {
+    const ranges = mergeRanges(input.ranges).sort((a, b) => a.lower - b.lower)
+    const numberOfIds = ranges.reduce((acc, curr) => {
+        const rangeSize = (curr.upper - curr.lower) + 1;
+        // console.log(`[${curr.lower}, ${curr.upper}] has range size ${rangeSize}`)
+        return acc + rangeSize;
+    }, 0);
+
+    const numberOfIdsUnmerged = input.ranges.reduce((acc, curr) => {
+        const rangeSize = (curr.upper - curr.lower) + 1;
+        return acc + rangeSize;
+    }, 0);
+
+    console.log('merged', numberOfIds, 'unmerged', numberOfIdsUnmerged)
     
-    return fileContents;
+    return numberOfIds;
+}
+
+export function mergeRanges(unmergedRanges: Range[]): Range[] {
+    if (unmergedRanges.length < 2) return unmergedRanges;
+
+    const mergedRanges: Range[] = [];
+    const compactedRangeIndexes = new Set<number>();
+    let range: Range|undefined;
+
+    for (let i = 0; i < unmergedRanges.length; i++) {
+        if (compactedRangeIndexes.has(i)) continue;
+        let wasCompacted = false;
+        range = unmergedRanges[i]!;
+        do {
+            wasCompacted = false;
+            for (let j = 0; j < unmergedRanges.length; j++) {
+                if (compactedRangeIndexes.has(j)) continue;
+                if (i === j) continue;
+                const overlaps = isOverlapping(range, unmergedRanges[j]!);
+                if (overlaps) {
+                    range = compactRanges(range, unmergedRanges[j]!)
+                    compactedRangeIndexes.add(j)
+                    wasCompacted = true;
+                }
+            }
+        } while (wasCompacted);
+
+        mergedRanges.push(range);
+    }
+
+    console.log('compacted', compactedRangeIndexes.size, 'from total', unmergedRanges.length, 'leaving', mergedRanges.length)
+
+    return mergedRanges;
+}
+
+function compactRanges(first: Range, second: Range): Range {
+    let compacted: Range | undefined = undefined;
+
+    // first is subsumed in second
+    if (first.lower >= second.lower && first.upper <= second.upper) {
+        compacted = second;
+    }
+     // second is subsumed in first
+    if (second.lower >= first.lower && second.upper <= first.upper) {
+        compacted = first;
+    }
+    
+    // first extends second lower
+    if (first.lower < second.lower && first.upper <= second.upper) {
+        compacted = Range.fromJSON({
+            lower: first.lower,
+            upper: second.upper,
+        });
+    }
+    // second extends first lower
+    if (second.lower < first.lower && second.upper <= first.upper) {
+        compacted = Range.fromJSON({
+            lower: second.lower,
+            upper: first.upper,
+        });
+    }
+
+    // first extends second upper
+    if (first.lower > second.lower && first.lower < second.upper && first.upper > second.upper) {
+        compacted = Range.fromJSON({
+            lower: second.lower,
+            upper: first.upper,
+        });
+    }
+    // second extends first upper
+    if (second.lower > first.lower && second.lower < first.upper && second.upper > first.upper) {
+        compacted = Range.fromJSON({
+            lower: first.lower,
+            upper: second.upper,
+        });
+    }
+
+    if (typeof compacted === 'undefined') throw new Error();
+
+    return compacted;
+}
+
+export function isOverlapping(range: IRange, other: IRange): boolean {
+    return (range.lower >= other.lower && range.lower <= other.upper) 
+        || (range.upper >= other.lower && range.upper <= other.upper)
+        || (other.lower >= range.lower && other.lower <= range.upper) 
+        || (other.upper >= range.lower && other.upper <= range.upper)
+}
+
+export function getInput(filename: string): IInput {
+    const fileContents = fs.readFileSync(filename, { encoding: 'utf8' });
+    const lines = fileContents.split('\n');
+    const input: IInput = {
+        ranges: [],
+        ids: [],
+    }
+    let reading: 'RANGE' | 'ID' = 'RANGE';
+    lines.forEach(l => {
+        if (l === '') {
+            reading = 'ID';
+            return;
+        }
+
+        if (reading === 'RANGE') {
+            const parts = l.split('-');
+            if (parts.length !== 2) throw new Error();
+            input.ranges.push(
+                new Range(
+                    parseInt(parts[0]!, 10),
+                    parseInt(parts[1]!, 10)
+                )
+            );
+        }
+
+        if (reading === 'ID') {
+            input.ids.push(parseInt(l, 10));
+        }
+    })
+    return input;
 }
--- day5/part2/test.ts
+++ day5/part2/test.ts
@@ -1,14 +1,42 @@
-import { solve, getInput } from './index.ts';
-import assert from 'node:assert';
+import { getInput, isOverlapping, mergeRanges, solve } from "./index.ts";
+import assert from "node:assert";
 
-Deno.test('day x, part y - test input', () => {
-    const input = getInput('./test-input.ts')
+Deno.test("day 5, part 2 - test input", () => {
+    const input = getInput("./test-input.txt");
     const res = solve(input);
-    assert.equal(res, 0)
-})
+    // console.log(res)
+    assert.equal(res, 14);
+});
 
-Deno.test('day x, part y - puzzle input', () => {
-    const input = getInput('../puzzle-input.ts')
+Deno.test("mergeRanges - test input", () => {
+    const input = getInput("./test-input.txt");
+    const res = mergeRanges(input.ranges);
+    const expected = [
+        { lower: 3, upper: 5 },
+        { lower: 10, upper: 20 },
+    ];
+    assert.deepStrictEqual(res.map((r) => r.toJSON()), expected);
+});
+
+Deno.test("mergeRanges - puzzle input", () => {
+    const input = getInput("../puzzle-input.txt");
+    const res = mergeRanges(input.ranges).sort((a, b) => a.lower - b.lower);
+
+    const overlaps = res.filter((r, i1) => {
+        let hasOverlap = false;
+        res.forEach((r2, i2) => {
+            if (i1 !== i2 && isOverlapping(r, r2)) {
+                console.log`${i1} overlaps with ${i2}: ${r}, ${r2}`;
+                hasOverlap = true;
+            }
+        });
+        return hasOverlap;
+    });
+    assert.equal(overlaps.length, 0)
+});
+
+Deno.test('day 5, part 2 - puzzle input', () => {
+    const input = getInput('../puzzle-input.txt')
     const res = solve(input);
-    assert.equal(res, 0)
+    assert.equal(res, 347338785050515)
 })

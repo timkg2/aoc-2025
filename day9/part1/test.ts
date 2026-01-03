8fcdaa28b9e723aa47731228a01d1104c4c8ea99
Tim KG <tim.theodor@gmail.com>
2026-01-03 03:12:06 +0100

    WIP day 9, part 2

--- day9/part1/test.ts
+++ day9/part1/test.ts
@@ -2,12 +2,6 @@
 import { solve, getInput } from './index.ts';
 import assert from 'node:assert';
 
-// Deno.test('day x, part 1 - get input', () => {
-//     const input = getInput('./test-input.txt')
-//     const expected = {}
-//     assert.deepStrictEqual(input, expected)
-// })
-
 Deno.test('day x, part 1 - solve test input', () => {
     const input = getInput('./test-input.txt')
     const res = solve(input);
@@ -17,5 +11,5 @@ Deno.test('day x, part 1 - solve test input', () => {
 Deno.test('day x, part 1 - solve puzzle input', () => {
     const input = getInput('../puzzle-input.txt')
     const res = solve(input);
-    assert.equal(res, 0)
+    assert.equal(res, 4782151432)
 })
--- day9/part2/test.ts
+++ day9/part2/test.ts
@@ -1,17 +1,57 @@
 /// <reference lib="deno.ns" />
-import { solve, getInput } from './index.ts';
+import { solve, getInput, type IVec2, type IPolygon } from './index.ts';
 import assert from 'node:assert';
+import { isInside, segmentsIntersect } from './intersection.ts';
 
-Deno.test('day x, part 2 - get input', () => {
-    const input = getInput('./test-input.txt')
-    const expected = {}
-    assert.deepStrictEqual(input, expected)
+Deno.test('segmentsIntersect()', () => {
+    const p1: IVec2 = { x: 0, y: 0 };
+    const p2: IVec2 = { x: 5, y: 5 };
+    
+    const q1: IVec2 = { x: 5, y: 0 };
+    const q2: IVec2 = { x: 0, y: 5 };
+    
+    const res = segmentsIntersect(p1, p2, q1, q2)
+    assert.equal(res, true)
 })
+
+Deno.test('segmentsIntersect()', () => {
+    const p1: IVec2 = { x: 0, y: 0 };
+    const p2: IVec2 = { x: 5, y: 0 };
+    
+    const q1: IVec2 = { x: 0, y: 5 };
+    const q2: IVec2 = { x: 5, y: 5 };
+    
+    const res = segmentsIntersect(p1, p2, q1, q2)
+    assert.equal(res, false)
+})
+
+// Deno.test('isInside()', () => {
+
+//     const rect: IPolygon = {
+//         vertices: [
+//             { x: 1, y: 1},
+//             { x: 5, y: 1},
+//             { x: 5, y: 5},
+//             { x: 1, y: 5}
+//         ]
+//     }
+
+//     assert.equal(isInside({ x: 3, y: 3 }, rect), true)
+//     assert.equal(isInside({ x: 1, y: 1 }, rect), true)
+//     assert.equal(isInside({ x: 5, y: 1 }, rect), true)
+//     assert.equal(isInside({ x: 5, y: 5 }, rect), true)
+//     assert.equal(isInside({ x: 1, y: 5 }, rect), true)
+
+//     assert.equal(isInside({ x: 3, y: 6 }, rect), false)
+//     assert.equal(isInside({ x: 0, y: 6 }, rect), false)
+//     assert.equal(isInside({ x: 6, y: 3 }, rect), false)
+//     assert.equal(isInside({ x: 0, y: 0 }, rect), false)
+// })
 
 Deno.test('day x, part 2 - solve test input', () => {
     const input = getInput('./test-input.txt')
     const res = solve(input);
-    assert.equal(res, 0)
+    assert.equal(res, 24)
 })
 
 Deno.test('day x, part 2 - solve puzzle input', () => {
